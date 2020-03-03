dd82f61f7e77f3d4726bc233693a2d28f8e407b2
Könönen Jesse <bgg255@myy.haaga-helia.fi>
2020-03-03 13:31:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sing-in form</title>
+    </head>
+    <body>
+        <h1>Sing in to Cards application</h1>
+        <table>
+            <form action="login.php" method="post">
+                <tr>
+                    <td>Username</td>
+                    <td><input name="username" type="text"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input name="password" type="password"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="login" value="Login"></td>
+                </tr>
+            </form>
+        </table>
+    </body>
+</html>
